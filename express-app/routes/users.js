b78ecd0e269203a701a8b8f1aaa1131fe15cac9f
Julia Farkun <julia.farkun@yandex.ru>
2026-03-24 17:18:13 +0500

    Task 0-4

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json({
+    items: [
+      { id: 1, name: 'Чванов Фёдор' },
+      { id: 2, name: 'Фаркун Юлия' }
+    ]
+  });
 });
 
 module.exports = router;
